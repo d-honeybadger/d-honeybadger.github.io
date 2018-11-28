1851cabecedba66243326e0df988e78fd4648982
Darya Komsa <komsa.darya@gmail.com>
2018-11-28 13:46:32 -0500

    Project videos, social links

---
+++ speedUpVideos.js
@@ -0,0 +1,9 @@
+function speedUpVideos() {
+  let pangolinVideo = document.getElementById("pangolin_video");
+  pangolinVideo.playbackRate = 2;
+  pangolinVideo.volume = 0.5;
+
+  let uwochemVideo = document.getElementById("uwochem_video");
+  uwochemVideo.playbackRate = 1.5;
+  uwochemVideo.volume = 0.5;
+}
