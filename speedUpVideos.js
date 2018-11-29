0a6f728f6484048f912cdac93eaa21f1555f2c50
Darya Komsa <komsa.darya@gmail.com>
2018-11-28 21:11:32 -0500

    Fixed next button alignment, updated resume

--- speedUpVideos.js
+++ speedUpVideos.js
@@ -1,9 +1,9 @@
 function speedUpVideos() {
   let pangolinVideo = document.getElementById("pangolin_video");
   pangolinVideo.playbackRate = 2;
-  pangolinVideo.volume = 0.5;
+  pangolinVideo.volume = 0;
 
   let uwochemVideo = document.getElementById("uwochem_video");
   uwochemVideo.playbackRate = 1.5;
-  uwochemVideo.volume = 0.5;
+  uwochemVideo.volume = 0;
 }
