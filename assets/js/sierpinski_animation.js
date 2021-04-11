31e268bf28152bed62610fbb360d06125d59914f
d-honeybadger <komsa.darya@gmail.com>
2021-04-11 09:51:54 -0400

    add landing page animation

--- assets/js/sierpinski_animation.js
+++ assets/js/sierpinski_animation.js
@@ -7,19 +7,15 @@ function drawTriangle() {
 
     // size and position the canvas
     const cos60 = Math.sqrt(3)/2;
-    if (window.innerWidth < window.innerHeight) {
-      canvas.width = 0.9*window.innerWidth;
-      canvas.height = cos60*canvas.width;
-    } else {
-      canvas.height = window.innerHeight - 50;
-      canvas.width = canvas.height/(cos60);
-    }
-    canvas.style.left = String(Math.floor((window.innerWidth - canvas.width)/2)) + 'px';
+    canvas.style.width ='100%';
+    canvas.width  = canvas.offsetWidth;
+    canvas.height = cos60*canvas.width;
 
     // animation parameters
     const sideLimit = Math.max(30, canvas.width/Math.pow(2, 5));
     let animationProgress = 0;
-    const animationSpeed = 13;
+    const baseAnimationSpeed = 10; // for a side of 100px
+    let animationSpeed = baseAnimationSpeed;
     const fps = 25;
     // params for the first (largest) triangle
     let origin = { x: canvas.width/2, y: canvas.height };
@@ -28,10 +24,13 @@ function drawTriangle() {
     const c = canvas.getContext('2d');
     // Helpers for drawing and animating
     function resetAnimation() {
-      console.log('resetting');
       c.clearRect(0, 0, canvas.width, canvas.height);
       drawBackground();
       animationProgress = 0;
+    }
+
+    function setAnimationSpeed(sideLength) {
+      animationSpeed = baseAnimationSpeed * sideLength / 100;
     }
 
     function drawTriangle(origin, sideLength, pointingUp, maxLineLength) {
@@ -59,26 +58,29 @@ function drawTriangle() {
     //level 1, 2, ...
     function drawTrianglesRecursively(origin, sideLength,  maxLineLength) {
       if (maxLineLength === undefined)  maxLineLength = Infinity;
+      // if animating, reset once all triangles are drawn
+      if (isFinite(maxLineLength) && sideLength < sideLimit && maxLineLength >= 0) {
+        resetAnimation();
+        return sideLength;
+      }
       //limit recursion by the size of the smallest triangle
-      if (sideLength < sideLimit || maxLineLength <= 0) return;
+      if (sideLength < sideLimit || maxLineLength <= 0) return sideLength;
+
       maxLineLength = drawTriangle(origin, sideLength, true, maxLineLength);
 
       // draw 3 triangles, one on each side
       sideLength = sideLength/2;
-      // if animating, reset once all triangles are drawn
-      //console.log('sideLength, sideLimit', sideLength, sideLimit);
-      if (isFinite(maxLineLength) && sideLength < sideLimit && maxLineLength >= 0) {
-        resetAnimation();
-        return;
-      }
+      
       let origins = [
         { x: origin.x - sideLength, y: origin.y },
         { x: origin.x, y: origin.y + 2*sideLength*cos60},
         { x: origin.x + sideLength, y: origin.y}
       ];
+      let currAnimationSideLength
       for (let i = 0; i < origins.length; i++) {
-        drawTrianglesRecursively(origins[i], sideLength, maxLineLength);
+        currAnimationSideLength = drawTrianglesRecursively(origins[i], sideLength, maxLineLength);
       }
+      return currAnimationSideLength
     }
 
     //Background - draw triangle without animation
@@ -92,12 +94,22 @@ function drawTriangle() {
 
     //Animate drawing
     function paintSierpinski() {
-      c.strokeStyle = '#639033';
+      c.strokeStyle = '#2997ff';
       c.beginPath();
+      
       let maxLineLength = drawTriangle(origin, sideLength, false, animationProgress);
-      drawTrianglesRecursively({ x: sideLength/2, y: 0}, sideLength/2, maxLineLength);
-      c.stroke();
+      let currSideLength = sideLength
+      currSideLength = drawTrianglesRecursively({ x: sideLength/2, y: 0}, sideLength/2, maxLineLength);
+
+      setAnimationSpeed(currSideLength)
       animationProgress += animationSpeed;
+
+      maxLineLength = drawTriangle(origin, sideLength, false, animationProgress);
+      currSideLength = sideLength
+      currSideLength = drawTrianglesRecursively({ x: sideLength/2, y: 0}, sideLength/2, maxLineLength);
+      
+      c.stroke();
+      
       setTimeout(function() { //slow down the animation to reduce CPU load
         window.requestAnimationFrame(paintSierpinski);
       }, 1000/fps);
@@ -105,8 +117,6 @@ function drawTriangle() {
 
 
     // Set up canvas parameters (clear in case we're resizing)
-    //c.clearRect(0, 0, window.innerWidth, window.innerHeight);
-    //resetAnimation();
     c.lineWidth = 1;
     c.lineJoin = 'bevel';
 
